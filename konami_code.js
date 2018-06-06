d6eb250f1cc8b7c5f612902becc17ed92b2c05de
Harry Dibden <harry-dibden@hotmail.com>
2018-06-06 12:24:22 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -11,6 +11,19 @@ const codes = [
   "a"
 ];
 
-function init() {
-  // your code here
+const body = document.querySelector("body");
+
+function init(e) {
+  const key = e.key;
+  let i = 0;
+  if(key === codes[i]){
+    i++;
+    if(i === codes.length){
+      alert("Well Done!");
+    }
+  } else {
+    i = 0;
+  }
 }
+
+body.addEventListener('onkeydown', init(e));
